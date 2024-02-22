debce40854b7a49f4a1dd1ac6375b5f0c02a1af4
Mahmudul Hasan <rakibahmadmr@gmail.com>
2024-02-22 13:59:55 +0600

    added auth layout

---
+++ src/app/(main)/auth/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="grainy w-full h-full flex items-center justify-center">
+      {children}
+    </main>
+  );
+}
