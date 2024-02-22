5edfcf594df87fb5d30be7a3feaee110939b9fec
Mahmudul Hasan <rakibahmadmr@gmail.com>
2024-02-22 12:39:50 +0600

    init portify project

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Portify",
+    default: "Portify",
+  },
+  description: "Craft  a stunning website in just a minutes.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("font-sans antialiased", inter.variable)}>
+        <Toaster position="top-right" richColors />
+        {children}
+      </body>
+    </html>
+  );
+}
