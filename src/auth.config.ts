639c6de6ddb4ee04a26de5c6181d53ddcb4407b9
Mahmudul Hasan <rakibahmadmr@gmail.com>
2024-02-22 18:38:05 +0600

    credential done

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,10 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import Facebook from "next-auth/providers/facebook";
+import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { SignInSchema } from "@/validators/auth";
+import { getUserByEmail } from "@/utility/user";
+import bcrypt from "bcryptjs";
 
 export const authConfig = {
   pages: {
@@ -10,10 +13,24 @@ export const authConfig = {
   },
   callbacks: {},
   providers: [
-    Credentials({}),
-    Facebook({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    Credentials({
+      async authorize(credentials) {
+        const body = await SignInSchema.safeParseAsync(credentials);
+        if (!body.success) {
+          return null;
+        }
+        const { email, password } = body.data;
+        const user = await getUserByEmail(email);
+        if (!user || !user.password || !user.emailVerified) return null;
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) return null;
+
+        return user;
+      },
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
